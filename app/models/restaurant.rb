1e024da642181d0f0ab70364bb303f964fae5851
John Ratana <jratana1@gmail.com>
2021-09-21 23:41:18 -0400

    associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+    has_many :likes, dependent: :destroy
+    has_many :liking_users, through: :likes, source: :user
+
+    belongs_to :owner, class_name: "User", foreign_key: "user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    
+    has_many :likes, dependent: :destroy
+    has_many :liked_restaurants, through: :likes, source: :restaurant
+
+    has_many :restaurants
 end
