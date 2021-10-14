bacbedc9f1539a43b9b39dc531c2969952581d34
John Ratana <jratana1@gmail.com>
2021-10-14 18:19:16 -0400

    checkout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,21 @@ class SessionsController < ApplicationController
     end
  
     def checkout
+        byebug
+        table= Table.find(params[:id])
 
         Stripe.api_key = ENV['STRIPE_API_KEY']
         session = Stripe::Checkout::Session.create({
+          customer_email: current_user.email,
           line_items: [{
             # TODO: replace this with the `price` of the product you want to sell
             price_data: {
               currency: 'usd',
               product_data: {
-                name: 'T-shirt',
+                name: table.restaurant.name,
+                description "Table for #{table.seats} at #{table.time} on #{table.seating}"
               },
-              unit_amount: 2000,
+              unit_amount: 10000,
             },
             quantity: 1,
           }],
@@ -27,8 +31,8 @@ class SessionsController < ApplicationController
             'card',
           ],
           mode: 'payment',
-          success_url: 'http://localhost:3001/#/' + '?success=true',
-          cancel_url: 'http://localhost:3001/#/' + '?canceled=true',
+          success_url: "http://localhost:3001/#/order/success?session_id={CHECKOUT_SESSION_ID}",
+          cancel_url: 'http://localhost:3001/#/order/cancel' + '?canceled=true',
         })
 
         render json: {checkoutURL: session.url}
